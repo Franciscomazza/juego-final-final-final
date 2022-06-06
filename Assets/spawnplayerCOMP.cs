c6137ff8adeeed97c4ccd4dcc8af1e411c3b3952
Francisco <familiasmazza@gmail.com>
2022-06-06 19:12:52 -0300

    rebote del spawn player
    rebotes entre las paredes

--- Assets/spawnplayerCOMP.cs
+++ Assets/spawnplayerCOMP.cs
@@ -6,23 +6,38 @@ public class spawnplayerCOMP : MonoBehaviour
 
 {
     public float speed;
+    public bool toright;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        toright = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z < 1.8)
+        if (toright)
+       
             // -2 y 1.8
         {
             transform.position += new Vector3(0, 0, speed);
 
         }
-        
+        else
+        {
+            transform.position -= new Vector3(0, 0, speed);
+        }
+
+        if (transform.position.z > 2)
+        {
+            toright = false;
+        }
+        if (transform.position.z < -2.4)  
+        {
+            toright = true;
+        }
+
         //es en el eje Z el movimiento porque lka pista está al revez
     }
 }
