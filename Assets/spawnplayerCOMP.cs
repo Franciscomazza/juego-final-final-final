9ef2a4c9bcb53eb8f3a04590c33bc8fe7a9c5c33
Franciscomazza <102179009+Franciscomazza@users.noreply.github.com>
2022-06-07 15:59:17 -0300

    aparición del circulo

--- Assets/spawnplayerCOMP.cs
+++ Assets/spawnplayerCOMP.cs
@@ -7,7 +7,7 @@ public class spawnplayerCOMP : MonoBehaviour
 {
     public float speed;
     public bool toright;
-
+    public GameObject prefab1;
     // Start is called before the first frame update
     void Start()
     {
@@ -37,6 +37,16 @@ public class spawnplayerCOMP : MonoBehaviour
         {
             toright = true;
         }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("up arrow key is held down");
+        }
+        //if (transform.position.x > baseDer.transform.position.x - 1)
+        //{
+        //    toright = false;
+        //    GameObject clon;
+        //    clon = Instantiate(prefab1);
+        //    clon.transform.position = baseDer.transform.position + new Vector3(0, yOffset, 0);
 
         //es en el eje Z el movimiento porque lka pista está al revez
     }
