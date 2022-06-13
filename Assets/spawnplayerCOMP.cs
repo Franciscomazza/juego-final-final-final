0482cecd8855fcc2a6cba06fb74ece39f222139e
Franciscomazza <102179009+Franciscomazza@users.noreply.github.com>
2022-06-13 15:49:57 -0300

    Bowling Hecho faltan los pinos
    desarrollo de bowling

--- Assets/spawnplayerCOMP.cs
+++ Assets/spawnplayerCOMP.cs
@@ -3,51 +3,96 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class spawnplayerCOMP : MonoBehaviour
-
 {
-    public float speed;
+    public GameObject flecha;
+
+    public float throwPower;
+    public float throwSpeed;
+    int throwInfluence = 1;
+
     public bool toright;
-    public GameObject prefab1;
+    public float horSpeed;
+    int influence = 1;
+
+    public float rotSpeed;
+    int rotInfluence = 1;
+
+    Rigidbody RB;
+    bool setpos = false, setrot = false, setforce = false;
+
     // Start is called before the first frame update
     void Start()
     {
         toright = true;
+        flecha.SetActive(false);
+        RB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (toright)
-       
-            // -2 y 1.8
+        //flecha.SetActive(false / true);
+        if(setpos == false)
         {
-            transform.position += new Vector3(0, 0, speed);
-
+            transform.position += new Vector3(0, 0, 1) * influence * horSpeed * Time.deltaTime;
         }
-        else
+        if(setpos == true && setrot == true)
         {
-            transform.position -= new Vector3(0, 0, speed);
+            transform.eulerAngles += new Vector3(0,1,0) * rotInfluence * rotSpeed * Time.deltaTime;
+        }
+        if (setrot == true && setforce == true)
+        {
+            throwPower += throwInfluence * throwSpeed * Time.deltaTime;
         }
 
         if (transform.position.z > 2)
         {
+            influence *= -1;
             toright = false;
         }
-        if (transform.position.z < -2.4)  
+        if (transform.position.z < -2.4)
         {
+            influence *= -1;
             toright = true;
         }
-        if (Input.GetKey(KeyCode.Space))
-        {
-            Debug.Log("up arrow key is held down");
-        }
-        //if (transform.position.x > baseDer.transform.position.x - 1)
-        //{
-        //    toright = false;
-        //    GameObject clon;
-        //    clon = Instantiate(prefab1);
-        //    clon.transform.position = baseDer.transform.position + new Vector3(0, yOffset, 0);
 
-        //es en el eje Z el movimiento porque lka pista está al revez
+        if(transform.eulerAngles.y > 125)
+        {
+            rotInfluence *= -1;
+        }
+        if(transform.eulerAngles.y < 55)
+        {
+            rotInfluence *= -1;
+        }
+
+        if(throwPower > 30)
+        {
+            throwInfluence *= -1;
+        }
+        if(throwPower < 10)
+        {
+            throwInfluence *= -1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            setpos = true;
+            if(setpos == true)
+            {
+                horSpeed =   0;
+                flecha.SetActive(true);
+                if (setrot == true)
+                {
+                    rotSpeed = 0;
+                    if (setforce == true)
+                    {
+                        RB.AddForce(transform.forward * throwPower, ForceMode.Impulse);
+                        flecha.SetActive(false);
+                    }
+                    setforce = true;
+                }
+                setrot = true;
+            }
+        }
     }
 }
