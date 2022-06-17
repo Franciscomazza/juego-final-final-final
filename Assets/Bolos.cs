b3709d944f6edc449db37d5e2bf49655694bf35a
IgnacioPagella <nacho.pagella@gmail.com>
2022-06-17 18:55:20 -0300

    Restart

---
+++ Assets/Bolos.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bolos : MonoBehaviour
+{
+    public GameObject Bola;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Bola.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "bolos")
+        {
+            Bola.SetActive(true);
+        }
+    }
+}
