447a6dfeb2c9e4ebeee065dc292ce0885470a05a
Franciscomazza <102179009+Franciscomazza@users.noreply.github.com>
2022-06-21 14:07:46 -0300

    update

--- Assets/Bolos.cs
+++ Assets/Bolos.cs
@@ -1,21 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Bolos : MonoBehaviour
 {
     public GameObject Bola;
+    public Text txtganaste;
+    public GameObject camara;
+    public Text txtgameover;
+    public int Contador;
+    public GameObject ObjectToClone;
     // Start is called before the first frame update
     void Start()
     {
+        txtgameover.enabled = false;
+        camara.SetActive(false);
+        txtganaste.enabled = false;
         Bola.SetActive(true);
     }
 
-    private void OnCollisionEnter(Collision col)
+    private void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "bolos")
         {
+            txtgameover.enabled = true;
             Bola.SetActive(false);
+            camara.SetActive(true);
+            txtganaste.enabled = true;
+        }
+        else
+        {
+            txtgameover.enabled = true;
+            camara.SetActive(true);
+            while (Contador < 10)
+            {
+                Instantiate(ObjectToClone);
+                ObjectToClone.transform.position = new Vector3(0, 50, 0);
+                Contador++;
+            }
         }
     }
 }
